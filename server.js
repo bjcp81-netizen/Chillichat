c68c2ee54c723c600959858dc2f9722fd7b6ba06
bjcp81 <bjcp81@gmail.com>
2026-07-25 11:18:48 +0100

    Use Render's dynamic port

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`ChilliChat server running at http://localhost:${PORT}`);
 });
