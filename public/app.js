01874d7e47e64ce86fe3fa579ee7c670d2c20e09
bjcp81 <bjcp81@gmail.com>
2026-08-06 09:51:07 +0100

    Silence harmless AbortError on rapid voice clip play/pause

--- public/app.js
+++ public/app.js
@@ -1038,7 +1038,7 @@ function isRecordingSupported() {
         audio.pause();
       } else {
         playSound(playClipSound);
-        audio.play();
+        audio.play().catch(() => {});
       }
     });
 
